78608ea0363a093bc217bcec4f87187ad1b2de8e
Adriel Klein <adriel@pillar.app>
2020-04-13 16:26:51 -0400

    use custom hook

--- src/pages/species/species_list.js
+++ src/pages/species/species_list.js
@@ -1,33 +1,10 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { compose, path } from "ramda";
+import React from "react";
 import { List } from "../../components";
 import { SPECIES_URL } from "../../constants";
 import { build_items } from "../../util";
-
-const process_results = data => {
-  return compose(path(["data", "results"]))(data);
-};
+import { useItems } from "../../util/hooks";
 
 export const SpeciesList = () => {
-  const [species, setSpecies] = useState([]);
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    async function fetch() {
-      const result = await axios(`${SPECIES_URL}?page=${page}`);
-      const new_species = process_results(result);
-      const full_list = [...species, ...new_species];
-      setSpecies(full_list);
-      if (result.data.next) {
-        setPage(page + 1);
-      }
-    }
-    // add the then() to get rid of the warnings.
-    fetch().then();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return <List>{build_items("species/", species)}</List>;
+  const items = useItems(SPECIES_URL);
+  return <List>{build_items("species/", items)}</List>;
 };
